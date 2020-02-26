06c627aa91a390bdbd100dc5573c7686fd2dfda0
Jhosevic Castañeda <eien@wark.io>
2020-02-26 19:01:25 +0100

    Changed files
    now its a separated file, so it can change the displayed ip between the public and the private

---
+++ proton_info.sh
@@ -0,0 +1,26 @@
+t=0
+
+toggle() {
+    t=$(((t + 1) % 2))
+}
+
+
+trap "toggle" USR1
+
+while true; do
+
+	local_addr=$(hostname -I | sed 's/^[ \t]*//;s/[ \t]*$//');
+	proton_status=$(protonvpn status);
+	current_server=$(grep -oEi '[a-zA-Z]*#\w+' <<< "$proton_status");
+	current_status=$(grep -oEi '(Connected|Disconnected)' <<< "$proton_status");
+	current_proton_ip=$(grep -oEi '[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*' <<< "$proton_status");
+
+    if [ $t -eq 0 ]; then
+		echo "$current_status ($local_addr) $current_server";
+    else
+		echo "$current_status ($current_proton_ip) $current_server";
+    fi
+    sleep 1 &
+    wait
+done
+
--- readme.md
+++ readme.md
@@ -10,6 +10,7 @@ Just a "for fun" project.
 
 ## Install
 Copy the content of `config` to your Polybar config file and add `proton_connection_status` to the desired position in you status bar.
+Then copy the `proton_info.sh` file to your desired location. By default is configured to `/home/your_user/.config/polybar/proton_info.sh`, if you place it somewhere else, change that route in the config file.
 
 ## Disclaimer
 I'm pretty bad at bash in general. k thx
